f0ad77d3547f7bb140550f5728cb269c46b30205
wangxiaoxiwudi <wxx.breaking@gmail.com>
2014-11-25 23:06:07 +0800

    two

--- testffmpeg/testffmpeg/testffmpeg.cpp
+++ testffmpeg/testffmpeg/testffmpeg.cpp
@@ -2,10 +2,15 @@
 //
 
 #include "stdafx.h"
-
+extern "C"
+{
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavutil/avutil.h>
+};
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	return 0;
+  return 0;
 }
 
